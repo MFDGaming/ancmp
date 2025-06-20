86495a0df37d01545f15e62e622bc57533a7e0ef
MFDGaming <alexandros.argentakis@gmail.com>
2025-06-20 18:58:30 +0300

    fixed possible stack alignment issues with the sysv wrappers

--- ancmp/abi_fix.h
+++ ancmp/abi_fix.h
@@ -24,70 +24,106 @@
 
 #define SYSV_WRAPPER_1(FUNC) \
     void __declspec(naked) SYSV_WRAPPER_##FUNC() { \
-        __asm push [esp+4] \
+        __asm push ebx \
+        __asm mov ebx, esp \
+        __asm sub esp, 4 \
+        __asm and esp, 0xFFFFFFF0 \
+        __asm add esp, 4 \
+        __asm push [ebx+8] \
         __asm call FUNC \
-        __asm pop eax \
+        __asm mov eax, [ebx+8] \
+        __asm mov esp, ebx \
+        __asm pop ebx \
         __asm retn 4 \
     }
 
 #define SYSV_WRAPPER_2(FUNC) \
     void __declspec(naked) SYSV_WRAPPER_##FUNC() { \
-        __asm push [esp+8] \
-        __asm push [esp+8] \
+        __asm push ebx \
+        __asm mov ebx, esp \
+        __asm sub esp, 8 \
+        __asm and esp, 0xFFFFFFF0 \
+        __asm add esp, 8 \
+        __asm push [ebx+12] \
+        __asm push [ebx+8] \
         __asm call FUNC \
-        __asm pop eax \
-        __asm add esp, 4 \
+        __asm mov eax, [ebx+8] \
+        __asm mov esp, ebx \
+        __asm pop ebx \
         __asm retn 4 \
     }
 
 #define SYSV_WRAPPER_3(FUNC) \
     void __declspec(naked) SYSV_WRAPPER_##FUNC() { \
-        __asm push [esp+12] \
-        __asm push [esp+12] \
-        __asm push [esp+12] \
+        __asm push ebx \
+        __asm mov ebx, esp \
+        __asm sub esp, 12 \
+        __asm and esp, 0xFFFFFFF0 \
+        __asm add esp, 12 \
+        __asm push [ebx+16] \
+        __asm push [ebx+12] \
+        __asm push [ebx+8] \
         __asm call FUNC \
-        __asm pop eax \
-        __asm add esp, 8 \
+        __asm mov eax, [ebx+8] \
+        __asm mov esp, ebx \
+        __asm pop ebx \
         __asm retn 4 \
     }
 
 #define SYSV_WRAPPER_4(FUNC) \
     void __declspec(naked) SYSV_WRAPPER_##FUNC() { \
-        __asm push [esp+16] \
-        __asm push [esp+16] \
-        __asm push [esp+16] \
-        __asm push [esp+16] \
+        __asm push ebx \
+        __asm mov ebx, esp \
+        __asm sub esp, 16 \
+        __asm and esp, 0xFFFFFFF0 \
+        __asm add esp, 16 \
+        __asm push [ebx+20] \
+        __asm push [ebx+16] \
+        __asm push [ebx+12] \
+        __asm push [ebx+8] \
         __asm call FUNC \
-        __asm pop eax \
-        __asm add esp, 12 \
+        __asm mov eax, [ebx+8] \
+        __asm mov esp, ebx \
+        __asm pop ebx \
         __asm retn 4 \
     }
 
 #define SYSV_WRAPPER_5(FUNC) \
     void __declspec(naked) SYSV_WRAPPER_##FUNC() { \
-        __asm push [esp+20] \
-        __asm push [esp+20] \
-        __asm push [esp+20] \
-        __asm push [esp+20] \
-        __asm push [esp+20] \
-        __asm push eax \
+        __asm push ebx \
+        __asm mov ebx, esp \
+        __asm sub esp, 20 \
+        __asm and esp, 0xFFFFFFF0 \
+        __asm add esp, 20 \
+        __asm push [ebx+24] \
+        __asm push [ebx+20] \
+        __asm push [ebx+16] \
+        __asm push [ebx+12] \
+        __asm push [ebx+8] \
         __asm call FUNC \
-        __asm pop eax \
-        __asm add esp, 16 \
+        __asm mov eax, [ebx+8] \
+        __asm mov esp, ebx \
+        __asm pop ebx \
         __asm retn 4 \
     }
 
 #define SYSV_WRAPPER_6(FUNC) \
     void __declspec(naked) SYSV_WRAPPER_##FUNC() { \
-        __asm push [esp+24] \
-        __asm push [esp+24] \
-        __asm push [esp+24] \
-        __asm push [esp+24] \
-        __asm push [esp+24] \
-        __asm push [esp+24] \
+        __asm push ebx \
+        __asm mov ebx, esp \
+        __asm sub esp, 24 \
+        __asm and esp, 0xFFFFFFF0 \
+        __asm add esp, 24 \
+        __asm push [ebx+28] \
+        __asm push [ebx+24] \
+        __asm push [ebx+20] \
+        __asm push [ebx+16] \
+        __asm push [ebx+12] \
+        __asm push [ebx+8] \
         __asm call FUNC \
-        __asm pop eax \
-        __asm add esp, 20 \
+        __asm mov eax, [ebx+8] \
+        __asm mov esp, ebx \
+        __asm pop ebx \
         __asm retn 4 \
     }
 
@@ -96,101 +132,117 @@
 #define SYSV_WRAPPER_1(FUNC) \
     void __attribute__((naked)) SYSV_WRAPPER_##FUNC() { \
         __asm__( \
-            "mov      4(%esp), %eax \n" \
-            "push %eax \n" \
+            "push %ebx \n" \
+            "mov %esp, %ebx \n" \
+            "sub $4, %esp \n" \
+            "and $0xFFFFFFF0, %esp \n" \
+            "add $4, %esp \n" \
+            "push 8(%ebx) \n" \
             "call " SYSV_WRAPPER_US #FUNC " \n" \
-            "pop %eax \n" \
-            "ret      $0x4 \n" \
+            "mov 8(%ebx), %eax \n" \
+            "mov %ebx, %esp \n" \
+            "pop %ebx \n" \
+            "ret $4 \n" \
         ); \
     }
 
 #define SYSV_WRAPPER_2(FUNC) \
-    void __attribute__((naked)) SYSV_WRAPPER_##FUNC() { \
+     void __attribute__((naked)) SYSV_WRAPPER_##FUNC() { \
         __asm__( \
-            "mov      8(%esp), %eax \n" \
-            "push %eax \n" \
-            "mov      8(%esp), %eax \n" \
-            "push %eax \n" \
+            "push %ebx \n" \
+            "mov %esp, %ebx \n" \
+            "sub $8, %esp \n" \
+            "and $0xFFFFFFF0, %esp \n" \
+            "add $8, %esp \n" \
+            "push 12(%ebx) \n" \
+            "push 8(%ebx) \n" \
             "call " SYSV_WRAPPER_US #FUNC " \n" \
-            "pop %eax \n" \
-            "add $4, %esp \n" \
-            "ret      $0x4 \n" \
+            "mov 8(%ebx), %eax \n" \
+            "mov %ebx, %esp \n" \
+            "pop %ebx \n" \
+            "ret $4 \n" \
         ); \
     }
 
 #define SYSV_WRAPPER_3(FUNC) \
     void __attribute__((naked)) SYSV_WRAPPER_##FUNC() { \
         __asm__( \
-            "mov      12(%esp), %eax \n" \
-            "push %eax \n" \
-            "mov      12(%esp), %eax \n" \
-            "push %eax \n" \
-            "mov      12(%esp), %eax \n" \
-            "push %eax \n" \
+            "push %ebx \n" \
+            "mov %esp, %ebx \n" \
+            "sub $12, %esp \n" \
+            "and $0xFFFFFFF0, %esp \n" \
+            "add $12, %esp \n" \
+            "push 16(%ebx) \n" \
+            "push 12(%ebx) \n" \
+            "push 8(%ebx) \n" \
             "call " SYSV_WRAPPER_US #FUNC " \n" \
-            "pop %eax \n" \
-            "add $8, %esp \n" \
-            "ret      $0x4 \n" \
+            "mov 8(%ebx), %eax \n" \
+            "mov %ebx, %esp \n" \
+            "pop %ebx \n" \
+            "ret $4 \n" \
         ); \
     }
 
 #define SYSV_WRAPPER_4(FUNC) \
     void __attribute__((naked)) SYSV_WRAPPER_##FUNC() { \
         __asm__( \
-            "mov      16(%esp), %eax \n" \
-            "push %eax \n" \
-            "mov      16(%esp), %eax \n" \
-            "push %eax \n" \
-            "mov      16(%esp), %eax \n" \
-            "push %eax \n" \
-            "mov      16(%esp), %eax \n" \
-            "push %eax \n" \
+            "push %ebx \n" \
+            "mov %esp, %ebx \n" \
+            "sub $16, %esp \n" \
+            "and $0xFFFFFFF0, %esp \n" \
+            "add $16, %esp \n" \
+            "push 20(%ebx) \n" \
+            "push 16(%ebx) \n" \
+            "push 12(%ebx) \n" \
+            "push 8(%ebx) \n" \
             "call " SYSV_WRAPPER_US #FUNC " \n" \
-            "pop %eax \n" \
-            "add $12, %esp \n" \
-            "ret      $0x4 \n" \
+            "mov 8(%ebx), %eax \n" \
+            "mov %ebx, %esp \n" \
+            "pop %ebx \n" \
+            "ret $4 \n" \
         ); \
     }
 
 #define SYSV_WRAPPER_5(FUNC) \
     void __attribute__((naked)) SYSV_WRAPPER_##FUNC() { \
         __asm__( \
-            "mov      20(%esp), %eax \n" \
-            "push %eax \n" \
-            "mov      20(%esp), %eax \n" \
-            "push %eax \n" \
-            "mov      20(%esp), %eax \n" \
-            "push %eax \n" \
-            "mov      20(%esp), %eax \n" \
-            "push %eax \n" \
-            "mov      20(%esp), %eax \n" \
-            "push %eax \n" \
+            "push %ebx \n" \
+            "mov %esp, %ebx \n" \
+            "sub $20, %esp \n" \
+            "and $0xFFFFFFF0, %esp \n" \
+            "add $20, %esp \n" \
+            "push 24(%ebx) \n" \
+            "push 20(%ebx) \n" \
+            "push 16(%ebx) \n" \
+            "push 12(%ebx) \n" \
+            "push 8(%ebx) \n" \
             "call " SYSV_WRAPPER_US #FUNC " \n" \
-            "pop %eax \n" \
-            "add $16, %esp \n" \
-            "ret      $0x4 \n" \
+            "mov 8(%ebx), %eax \n" \
+            "mov %ebx, %esp \n" \
+            "pop %ebx \n" \
+            "ret $4 \n" \
         ); \
     }
 
 #define SYSV_WRAPPER_6(FUNC) \
     void __attribute__((naked)) SYSV_WRAPPER_##FUNC() { \
         __asm__( \
-            "mov      24(%esp), %eax \n" \
-            "push %eax \n" \
-            "mov      24(%esp), %eax \n" \
-            "push %eax \n" \
-            "mov      24(%esp), %eax \n" \
-            "push %eax \n" \
-            "mov      24(%esp), %eax \n" \
-            "push %eax \n" \
-            "mov      24(%esp), %eax \n" \
-            "push %eax \n" \
-            "mov      24(%esp), %eax \n" \
-            "push %eax \n" \
+            "push %ebx \n" \
+            "mov %esp, %ebx \n" \
+            "sub $24, %esp \n" \
+            "and $0xFFFFFFF0, %esp \n" \
+            "add $24, %esp \n" \
+            "push 28(%ebx) \n" \
+            "push 24(%ebx) \n" \
+            "push 20(%ebx) \n" \
+            "push 16(%ebx) \n" \
+            "push 12(%ebx) \n" \
+            "push 8(%ebx) \n" \
             "call " SYSV_WRAPPER_US #FUNC " \n" \
-            "pop %eax \n" \
-            "add $20, %esp \n" \
-            "ret      $0x4 \n" \
+            "mov 8(%ebx), %eax \n" \
+            "mov %ebx, %esp \n" \
+            "pop %ebx \n" \
+            "ret $4 \n" \
         ); \
     }
 
